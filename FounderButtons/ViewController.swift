bc4840262d3ad1e479dea51f81ee5ce6b8c9a450
Xiaoyu Hu <hufk@bc.edu>
2019-01-27 22:49:02 -0500

    2.3 completed - founder button displays and clear names of founders of Google and Runway

--- FounderButtons/ViewController.swift
+++ FounderButtons/ViewController.swift
@@ -9,12 +9,28 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    //nameInMurral is the variable for the name on the top
+    @IBOutlet weak var nameInMurral: UILabel!
+    //nameInGold is the variable for the name at the bottom
+    @IBOutlet weak var nameInGold: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
 
+    @IBAction func buttonGooglePressed(_ sender: Any) {
+        nameInMurral.text = "Larry Page"
+        nameInGold.text = "Sergey Brin"
+    }
+    @IBAction func buttonRunwayPressed(_ sender: Any) {
+        nameInMurral.text = "Jennifer Hyman"
+        nameInGold.text = "Jenny Fleiss"
+    }
+    @IBAction func clearAllNamesPressed(_ sender: Any) {
+        nameInMurral.text = ""
+        nameInGold.text = ""
+    }
 }
 
